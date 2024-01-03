1731cdd2ebef798d3a8c0135c80919a2a9b18e7b
Ksenia Ivanova <ksenia.ivanovva@gmail.com>
2024-01-03 10:58:25 +0100

    Add default layout

---
+++ web/src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@chakra-ui/react'
+
+import Footer from 'src/components/Footer/Footer'
+import TopBar from 'src/components/TopBar/TopBar'
+
+type DefaultLayoutProps = {
+  children?: React.ReactNode
+}
+
+const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+  return (
+    <Box height="100vh" overflow="auto" w="100%">
+      <TopBar />
+      <Box as="main" overflowX="hidden" bg="rosybrown" pt={['80px', '110px']}>
+        {children}
+        <Footer />
+      </Box>
+    </Box>
+  )
+}
+
+export default DefaultLayout
