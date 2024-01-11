ede846fdd672fc96e058be020a7b2bb25ddbe28a
Ksenia Ivanova <ksenia.ivanovva@gmail.com>
2024-01-11 23:44:56 +0100

    Add conditional rendering for button

--- web/src/components/CustomButton/CustomButton.tsx
+++ web/src/components/CustomButton/CustomButton.tsx
@@ -4,7 +4,7 @@ import { Button } from '@chakra-ui/react'
 
 const CustomButton = ({ buttonText, onClick, id }) => {
   return (
-    <Button colorScheme="blue" size="md" onClick={onClick} id={id}>
+    <Button colorScheme="blue" size="md" onClick={onClick} id={id} mt={4} mb={4} w="200px">
       {buttonText}
     </Button>
   )
