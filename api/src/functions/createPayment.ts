737b1fcb87fd3fcd943e939217b4f4ecd0a50583
Ksenia Ivanova <ksenia.ivanovva@gmail.com>
2024-01-14 13:21:39 +0100

    Fix: add callbackURL

--- api/src/functions/createPayment.ts
+++ api/src/functions/createPayment.ts
@@ -40,7 +40,7 @@ export const handler = async (event: APIGatewayEvent, _context: Context) => {
           },
         ],
         merchantUrls: {
-          callbackUrl: "https://71a9-2-69-165-81.ngrok-free.app/.redwood/functions/paymentCallback"
+          callbackUrl: "https://zecond.vercel.app/.redwood/functions/paymentCallback"
 
         }
 
